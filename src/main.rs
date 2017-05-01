fd5b1300c6dac66c3842cad93a1e6edc860962aa
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-01 17:54:37 +0200

    add it all to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+extern crate ansi_term;
+
+#[macro_use]
+mod macros;
+use macros::*;
+
 mod example1;
+mod example2;
+mod example3;
+mod example4;
+mod example5;
+
+use ansi_term::Colour::*;
 
 fn main()
 {
+	println!("{}", Yellow.paint("Example 1 output:\n"));
     example1::example1();
+	println!("{}", Yellow.paint("Example 2 output:\n"));
+    example2::example2();
+	println!("{}", Yellow.paint("Example 3 output:\n"));
+	example3::example3();
 }
