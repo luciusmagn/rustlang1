e285491232de74e1fa4d98a3387f93c97a66deb4
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-01 17:25:01 +0200

    add a macro for printing stuff indented

---
+++ src/macros.rs
@@ -0,0 +1,17 @@
+//macro for indenting examples
+
+#![macro_escape]
+#[macro_export]
+macro_rules! xprintln {
+    () => (print!("\n"));
+    ($fmt:expr) =>
+    ({
+        print!("\t");
+        print!(concat!($fmt, "\n"));
+    });
+    ($fmt:expr, $($arg:tt)*) =>
+    ({
+        print!("\t");
+        print!(concat!($fmt, "\n"), $($arg)*);
+    });
+}
