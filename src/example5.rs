1ead2404c1009f28d35c73dc7b4c0d8d036e9e1b
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-03 12:19:22 +0200

    add example 5

--- src/example5.rs
+++ src/example5.rs
@@ -1,4 +1,33 @@
 pub fn example5()
 {
-	//continue at 14:43
+	let mut x = 1;
+	loop
+	{
+		if (x % 2) == 0
+	 	{
+	 		xprintln!("LOOP {}", x);
+	 		x+=1;
+
+	 		continue;
+	 	}
+		if x > 10
+		{
+			break;
+
+		}
+		x+=1;
+		continue;
+	}
+
+	let mut y = 1;
+	while y <= 10
+	{
+		xprintln!("WHILE {}", y);
+		y+=1
+	}
+
+	for z in 1..10
+	{
+		xprintln!("FOR {}", z);
+	}
 }
