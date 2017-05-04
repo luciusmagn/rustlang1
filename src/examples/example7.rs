01a148de2331e1147489f08ce2f2c016c9f69d82
Lukáš Hozda <luk.hozda@gmail.com>
2017-05-04 14:42:37 +0200

    add 7th example

---
+++ src/examples/example7.rs
@@ -0,0 +1,31 @@
+use std::io::stdin;
+
+pub fn example7()
+{
+	'outer: loop
+	{
+		let number: i32 = 10;
+		xprintln!("Pick a number");
+
+		loop
+		{
+			let mut line = String::new();
+			let input = stdin().read_line(&mut line);
+
+			let guess: Option<i32> = input.ok().map_or(None, |_| line.trim().parse().ok());
+
+			match guess
+			{
+				None => xprintln!("Enter a number"),
+				Some(n) if n == number =>
+				{
+					xprintln!("You guessed it");
+					break 'outer;
+				},
+				Some(n) if n < number => xprintln!("Too low"),
+				Some(n) if n > number => xprintln!("Too big"),
+				Some(_) => xprintln!("Error"),
+			}
+		}
+	}
+}
